b0fc19bf03362989d5b7a5e262a4cc9d7645393a
FilipeChagasDev <filipe.ferraz0@gmail.com>
2019-03-17 19:02:37 -0300

    código identado

--- projeto.c
+++ projeto.c
@@ -1,63 +1,67 @@
 #include<stdio.h>
 #include<string.h>
 #include<conio2.h>
+
 void tela()
 {
-textbackground(31);
-textcolor(19);
-gotoxy(1,1);
-printf("%c", 201);//canto superior esquerdo
-gotoxy(80,26);
-printf("%c", 204);//canto inferior direito
-int c;
-int botao;
-for(c = 2; c < 80; c++)//linhas superiores e inferiores na horizontal
-{
-	gotoxy(c,3);
-	printf("%c", 205);
-	gotoxy(c,1);
-	printf("%c", 205);
-	gotoxy(c,26);
-	printf("%c", 205);
-	gotoxy(c,24);
-	printf("%c", 205);
-}
-gotoxy(1,24);
-printf("%c", 204);//Junção esquerda
-gotoxy(80,1);
-printf("%c", 187);
-int l;
-for(l=2;l<26;l++)//linhas laterais na vertical
-{
-	gotoxy(1,l);
-	printf("%c",186);
-	gotoxy(80,l);
-	printf("%c",186);
-}
-gotoxy(80,24);
-printf("%c",185);
-gotoxy(80,1);
-printf("%c",187);
-gotoxy(1,26);
-printf("%c",200);
-gotoxy(1,3);
-printf("%c", 204);
-gotoxy(80,3);
-printf("%c", 185);
-//opções do usuario
-gotoxy(4,2);
-printf("Cadastro\t\Listar     Buscar     Atualizar     Sair");
-do
-{
+	textbackground(31);
+	textcolor(19);
+	gotoxy(1,1);
+	printf("%c", 201);//canto superior esquerdo
+	gotoxy(80,26);
+	printf("%c", 204);//canto inferior direito
+	int c;
+	int botao;
+	for(c = 2; c < 80; c++)//linhas superiores e inferiores na horizontal
+	{
+		gotoxy(c,3);
+		printf("%c", 205);
+		gotoxy(c,1);
+		printf("%c", 205);
+		gotoxy(c,26);
+		printf("%c", 205);
+		gotoxy(c,24);
+		printf("%c", 205);
+	}
+	gotoxy(1,24);
+	printf("%c", 204);//Junção esquerda
+	gotoxy(80,1);
+	printf("%c", 187);
+	int l;
+	for(l=2;l<26;l++)//linhas laterais na vertical
+	{
+		gotoxy(1,l);
+		printf("%c",186);
+		gotoxy(80,l);
+		printf("%c",186);
+	}
+	gotoxy(80,24);
+	printf("%c",185);
+	gotoxy(80,1);
+	printf("%c",187);
+	gotoxy(1,26);
+	printf("%c",200);
+	gotoxy(1,3);
+	printf("%c", 204);
+	gotoxy(80,3);
+	printf("%c", 185);
+	//opções do usuario
 	gotoxy(4,2);
+	printf("Cadastro\t\Listar     Buscar     Atualizar     Sair");
+	do
+	{
+		gotoxy(4,2);
 	
-}while();
+	}while(0);
  }
- void telamenu()
- {
- 	int botao;
+
+
+void telamenu()
+{
+	int botao;
  	int l;
  	int c;
+
  	for(l=4;l<12;l++)
  	{
  		//criação da tela de login
@@ -69,6 +73,7 @@ do
  		printf("%c", 187);
  		gotoxy(46,8);
  		printf("        Tela de Menu");
+ 		
  		for(l= 8; l<22; l++)
  		{
  			gotoxy(45, l);
@@ -76,6 +81,7 @@ do
  			gotoxy(75, l);
  			printf("%c", 186);
 		}
+
 		gotoxy(45,9);
 		printf("%c",204);
 		gotoxy(75,9);
@@ -88,6 +94,7 @@ do
 		printf("%c",200);
 		gotoxy(75,22);
 		printf("%c",188);
+		
 		for(c=46; c<75; c++)
 		{
 			gotoxy(c,7);
@@ -100,53 +107,61 @@ do
 			printf("%c",205);
 			gotoxy(c,25);
 			printf("%c", 205);		
-			}
- 	    }
- 	    for(l = 47; l< 70; l++)
- 	    {
- 	    	gotoxy(l, 13);
- 	    	printf("%c", 196);
- 	    	gotoxy(l, 17);
- 	    	printf("%c", 196);
-		 }
- 	    gotoxy(47,11);
- 	    printf("Usuario:");
- 	    gotoxy(47,15);
- 	    printf("Senha:");
- 	    gotoxy(46,21);
- 	    printf("  <A>1 Acesso <C> Cadastro   ");
- 	     for(l=22; l<26; l++)
-	    {
+		}
+ 	}
+ 	
+ 	for(l = 47; l< 70; l++)
+ 	{
+ 		gotoxy(l, 13);
+ 	  	printf("%c", 196);
+ 	   	gotoxy(l, 17);
+ 	   	printf("%c", 196);
+	
+	}
+ 	
+ 	gotoxy(47,11);
+ 	printf("Usuario:");
+ 	gotoxy(47,15);
+ 	printf("Senha:");
+ 	gotoxy(46,21);
+ 	printf("  <A>1 Acesso <C> Cadastro   ");
+ 	
+ 	for(l=22; l<26; l++)
+	{
 	 	gotoxy(45, l);
 		printf("%c", 186);
 		gotoxy(75, l);
 		printf("%c", 186); 
-	    }
-	    gotoxy(48,23);
-	    textcolor(36);
-	    textbackground(14);
-	    printf("E");
-	    textcolor(19);
-	    textbackground(31);
-	    printf("ntrar");
-	    gotoxy(65,23);
-	    textcolor(36);
-	    textbackground(14);
-	    printf("S");
-	    textcolor(19);
-	    textbackground(31);
-	    printf("air");
-	    botao = getch();
-	    if (botao == 97 || botao == 99)
-	   {
+	}
+
+	gotoxy(48,23);
+	textcolor(36);
+	textbackground(14);
+	printf("E");
+	textcolor(19);
+	textbackground(31);
+	printf("ntrar");
+	gotoxy(65,23);
+	textcolor(36);
+	textbackground(14);
+	printf("S");
+	textcolor(19);
+	textbackground(31);
+	printf("air");
+	botao = getch();
+
+	if (botao == 97 || botao == 99)
+	{
 	 	system("cls");
 	 	MenuCadastro();
-	   }
-	   if(botao == 115)
-	   {
-	   	exit(0);
-	   } 
-	 }
+	}
+
+	if(botao == 115)
+	{
+	   exit(0);
+	} 
+}
+
 void MenuCadastro()
 {
 	int l;
@@ -186,6 +201,7 @@ void MenuCadastro()
 		gotoxy(l, 24);
 		printf("%c", 196);
 	}
+	
 	for(l=35; l < 77; l++)
 	{
 		gotoxy(l, 4);
@@ -193,6 +209,7 @@ void MenuCadastro()
 		gotoxy(l, 26);
 		printf("%c",205);
 	}
+	
 	for(c=5; c< 26; c++)
 	{
 		gotoxy(34, c);
@@ -200,6 +217,7 @@ void MenuCadastro()
 		gotoxy(77, c);
 		printf("%c",186);
 	}
+	
 	gotoxy(10,10);
 }
 void submenu(int csub, int parametros)
@@ -232,8 +250,10 @@ void submenu(int csub, int parametros)
 				printf(" ");		
 		}//ta com bug que o getch não esta indo para onde deveria ir
 	}
+	
 	char botao;
 	int parametros2;
+	
 	do
 	{
 		gotoxy(csub+1,5);
@@ -263,7 +283,9 @@ void submenu(int csub, int parametros)
 			parametros2--;
 		}
 	}while(botao!=13&&botao!=27);
+	
 	textbackground(31);
+	
 	for(l = 0, mx=11,my=parametros+1; l<=my; l++)
 	{
 		gotoxy(csub,l+4);
@@ -273,8 +295,11 @@ void submenu(int csub, int parametros)
 		}
 	}
 }
- int main(void)
- {
+
+// -------------- funcao main ----------------
+
+int main(void)
+{
 	 /*
  	MenuCadastro();
  	system("pause");
@@ -288,4 +313,4 @@ void submenu(int csub, int parametros)
  	gotoxy(25,25);
  	system("pause");
  	return 0;
-  } 
+} 
